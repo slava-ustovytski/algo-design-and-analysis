33215a8a616f78df2d6d60e3456fc4714b9d5d7f
Slava Ustovytski <sustovytski@aconex.com>
2015-10-27 23:02:36 +1100

    improvements to merge sort impl :

--- week1/merge_sort/merge_sort.go
+++ week1/merge_sort/merge_sort.go
@@ -3,37 +3,52 @@ package main
 import "fmt"
 
 func main() {
-	unsorted := []int{5, 4, 1, 8, 7, 2, 6, 3}
+	// unsorted := []int{5, 4, 1, 8, 7, 2, 6, 3}
+	unsorted := []int{8, 7, 2, 1}
 	sorted := Sort(unsorted)
 	fmt.Println(unsorted, " => ", sorted)
 }
 
 func Sort(x []int) []int {
+	length := len(x)
+	if length <= 1 {
+		return x
+	}
 
-	half := len(x) / 2
+	half := length / 2
 	left := x[:half]
-	right := x[half:len(x)]
+	right := x[half:]
 
-	if len(x) > 2 {
+	if length > 2 {
 		left = Sort(left)
 		right = Sort(right)
 	}
 	sorted := merge(left, right)
+	
 	return sorted
 }
 
 func merge(left, right []int) []int {
-	var length int
-	length = len(left)
 	var sorted []int
-	for i := 0; i < length; i++ {
-		if left[i] < right[i] {
+	i, j := 0, 0
+	for i<len(left) && j<len(right) {
+		if left[i] < right[j] {
 			sorted = append(sorted, left[i])
-			sorted = append(sorted, right[i])
-		} else {
-			sorted = append(sorted, right[i])
-			sorted = append(sorted, left[i])
+			i++
+		} else  {
+			sorted = append(sorted, right[j])
+			j++
 		}
+	}
+
+	for i < len(left) {
+		sorted = append(sorted, left[i])
+		i++
+	}
+
+	for j < len(right) {
+		sorted = append(sorted, right[j])
+		j++
 	}
 
 	return sorted
--- week1/merge_sort/merge_sort_test.go
+++ week1/merge_sort/merge_sort_test.go
@@ -1,9 +1,30 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestReturnsSortedArray(t *testing.T) {
+func testReturnsSortedArray(t *testing.T) {
 	unsorted := []int{4, 3, 2, 1}
 	sorted := Sort(unsorted)
-	//t.Log("fuck")
+	if !reflect.DeepEqual(sorted, []int{1, 2, 3, 4}) {
+		t.Error("expected sorted array, got", sorted)
+	}
+}
+
+func TestSingleElementsArrayJustReturned(t *testing.T) {
+	unsorted := []int{5}
+	sorted := Sort(unsorted)
+	if !reflect.DeepEqual(sorted, unsorted) {
+		t.Error("single element array expected to be returned unchanged")
+	}
+}
+
+func TestSortElementsArray(t *testing.T) {
+	unsorted := []int{7, 8, 2, 1}
+	sorted := Sort(unsorted)
+	if !reflect.DeepEqual(sorted, []int{1, 2, 7, 8}) {
+		t.Error("expected sorted array, got", sorted)
+	}
 }
